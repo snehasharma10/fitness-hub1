fe1c6b1036c62850362ccd26692100295f069b29
snehasharma10 <162324037+snehasharma10@users.noreply.github.com>
2024-05-28 02:08:49 +0530

    Add files via upload

---
+++ login-page.js
@@ -0,0 +1,11 @@
+        function showRegisterForm() {
+            document.querySelector('.login-container').style.display = 'none';
+            document.querySelector('.register-container').style.display = 'block';
+        }
+
+        function showLoginForm() {
+            document.querySelector('.login-container').style.display = 'block';
+            document.querySelector('.register-container').style.display = 'none';
+        }
+
+
